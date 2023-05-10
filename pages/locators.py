4d48a3ae60a974983f0a7ab44853e134ebcb7c6d
wwladkar <wwladkar@gmail.com>
2023-05-10 08:23:46 +0300

    Added login_page.py and modified locators.py, test_main_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     GO_TO_BASKET_LINK = (By.CSS_SELECTOR, ".btn-group .btn-default:nth-child(1)")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -20,4 +21,11 @@ def test_guest_can_go_to_basket(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
 
